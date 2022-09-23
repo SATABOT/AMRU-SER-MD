2a162ae96061fb3090d6125aaf2bca0769d57d5d
SATABOT <113933058+SATABOT@users.noreply.github.com>
2022-09-23 22:57:38 +0330

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,14 +31,14 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['919895802105'] //ur owner number
+global.vcardowner = ['989175919318'] //ur owner number
 global.ownername = "AMRUSER" //ur owner name
 global.ytname = "YT: DRON" //ur yt chanel name
 global.socialm = "GitHub: AMRUSIR" //ur github or insta name
 global.location = "India, Kerala, Kottayam" //ur location
 
 //bot bomdy 
-global.owner = ['919895802105']
+global.owner = ['989175919318']
 global.ownertag = '919895802105' //ur tag number
 global.botname = 'AMRU-SER-MD' //ur bot name
 global.linkz = "" //your theme url which will be displayed on whatsapp
@@ -56,7 +56,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['919895801205'] //ur premium numbers
+global.premium = ['989175919318'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
